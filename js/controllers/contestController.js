20d4a8d08496ee6cc2686537a6f1c6b088617e10
Nossyra46 <nossyra@gmail.com>
2016-05-13 12:03:35 +0200

    add player

--- js/controllers/contestController.js
+++ js/controllers/contestController.js
@@ -8,15 +8,34 @@ app.controller('contestController', function($scope){
 			nbr_votes
 		}
 	*/
-	this.players = [
-	{
-		name: "harysson",
-		url: "",
-	}];
+	$scope.players = players;
+	console.log($scope.players[0]);
 	$scope.name = '';
 	$scope.artwork = '';
 
-	$scope.nbRows
+	$scope.subscribe = function() {
+		var player = {};
+		player.name = $scope.name;
+		player.url = "../img/"+$scope.artwork+".jpeg";
+		player.nbrVotes = 0;
+		console.log(player);
+		$scope.players.push(player);
+	}
 
+	$scope.isFirst = function(player){
+		return players.indexOf(player) % 3 === 0;
+	}
 
 });
+
+var players = [
+	{
+		"name": "yoann56",
+		"url": "../img/pic.jpg",
+		"nbrVotes": 0
+	},
+	{
+		"name": "celia91",
+		"url": "../img/pic2.jpg",
+		"nbrVotes": 0
+	}];
