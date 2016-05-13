6b16d1b27aeeea4c8eba5707b663a981063dabf2
Shinji <shinji51@hotmail.fr>
2016-05-13 12:02:17 +0200

    ajouts angular page profile

---
+++ js/controllers/ProfilController.js
@@ -0,0 +1,46 @@
+app.controller("ProfilController", ['$scope', '$http', function($scope) {
+	
+	$scope.users = users;
+	$scope.user = $scope.users[0];
+	this.users = users;
+	console.log($scope.user);
+}]);
+
+
+var users = [
+	{
+		"name": "yoann51",
+		"description": "Photographe amateur",
+		"hobbies": "Animes, rammstein, Bretagne, moto",
+		"gallery": [
+			"img/"
+		]
+
+	},
+	{
+		"name": "celia91",
+		"description": "Photographe amateur",
+		"hobbies": "NCIS, foot(PSG), Friends, Rap & Hip Hop US",
+		"gallery": [
+			"img/forêt.jpg",
+			"img/mountains.jpg",
+			"img/toits.jpg",
+			"img/cadenas.jpg",
+			"img/paysage.jpg",
+			"img/tour.jpg",
+		]
+	},
+	{
+		"name": "riri93",
+		"description": "Photographe amateur",
+		"hobbies": ["Teaching", "Musique", "Animes"],
+		"gallery": [
+			"img/pic1.jpeg",
+			"img/pic2.jpeg",
+			"img/pic3.jpeg",
+			"img/pic4.jpeg",
+			"img/pic5.jpeg",
+			"img/pic6.jpeg",
+		]
+	}
+];
--- js/controllers/profileController.js
+++
@@ -1,9 +0,0 @@
-app.controller("ProfilController", ['$scope', '$http', function($scope, $http) {
-	$http.jsonp('../js/data/users.json')
-	.success(function (data) {
-		$scope.data = data;
-		$scope.user = data[0];
-
-	});
-	
-}]);
